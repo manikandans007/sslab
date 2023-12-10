8128edc39afa84e1fa39c5c219d437c7e982ec86
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-12-10 13:03:05 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,51 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct{
- int pid;
- int btime;
- int atime;
- int wtime;
- int tatime;
-} sp;
-int main(){
- int i, j, n,ctime=0;
- float total_waiting_time=0, total_turnaround_time=0;
- sp *p, t;
- printf("Enter the no. of processes: ");
- scanf("%d", &n);
- p = (sp *)malloc(n * sizeof(sp));
- printf("Enter the burst time and arrival time of");
- for (i = 0; i < n; i++){
- printf("\nProcess%d\t:", i + 1);
- scanf("%d", &p[i].btime);
- scanf("%d", &p[i].atime);
- p[i].pid = i + 1;
- p[i].wtime = 0;
- }
- for(i=0;i<n;i++){
- for(j=0;j<n-i-1;j++){
- if(p[j].atime>p[j+1].atime){
- t=p[j];
- p[j]=p[j+1];
- p[j+1]=t;
+#include<stdio.h>
+struct Process {
+ int pid; 
+ int burst_time; 
+ int at;
+ int tat;
+ int waittime;
+};
+void fcfsScheduling(struct Process proc[], int n) {
+ int i, j;
+ struct Process temp;
+ for (i = 0; i < n - 1; i++) {
+ for (j = 0; j < n - i - 1; j++) {
+ if (proc[j].at > proc[j + 1].at) {
+ temp = proc[j];
+ proc[j] = proc[j + 1];
+ proc[j + 1] = temp;
+ } 
  }
  }
+ int total_waiting_time = 0;
+ int total_turnaround_time = 0;
+ int time=0;
+ i=0;
+while (i<n) {
+ if(proc[i].at<=time){
+ time+=proc[i].burst_time;
+ proc[i].tat=time-proc[i].at;
+ proc[i].waittime=proc[i].tat-proc[i].burst_time;
+ total_turnaround_time+=proc[i].tat;
+ total_waiting_time+=proc[i].waittime;
+ i++;
+ } 
+ Else{
+ time++;
+ } 
  }
- printf("\n|------------------------------Process Scheduling--------------------------------|");
- printf("\n|Process\t|Burst Time\t|Arrival Time\t|Waiting Time\t|Turn Around Time|");
- printf("\n|--------------------------------------------------------------------------------|");
- for (i = 0; i < n; i++){
- ctime += p[i].btime;
- p[i].tatime = ctime - p[i].atime;
- p[i].wtime = p[i].tatime - p[i].btime;
- total_waiting_time += p[i].wtime;
- total_turnaround_time += p[i].tatime;
- printf("\n|%d\t\t|%d\t\t|%d", p[i].pid, p[i].btime, p[i].atime);
- printf("\t\t|%d\t\t|%d \t\t |", p[i].wtime, p[i].tatime);
+ printf("Process\tBT\tAT\tWT \t TAT\n");
+ for (i = 0; i < n; i++) {
+ printf("%d\t%d\t%d\t%d\t%d\n", proc[i].pid, proc[i].burst_time,
+ proc[i].at,proc[i].waittime,proc[i].waittime+proc[i].burst_time);
  }
- printf("\n|--------------------------------------------------------------------------------|");
- printf("\n Total Waiting Time: %.2f", total_waiting_time);
- printf("\n Average Waiting Time: %.2f", (float)total_waiting_time / n);
- printf("\n Total Turn Around Time: %.2f", total_turnaround_time);
- printf("\n Average Turn Around Time: %.2f", (float)total_turnaround_time / n);
+ printf("\nAverage Waiting Time: %.2f\n", (float) total_waiting_time / n);
+ printf("Average Turnaround Time: %.2f\n", (float) total_turnaround_time / n);
+}
+void main() {
+int n, i;
+printf("Enter the number of processes: ");
+scanf("%d", &n);
+struct Process proc[n];
+printf("Enter process details (Process ID, Burst Time, Arrival Time):\n");
+for (i = 0; i < n; i++) {
+printf("Process %d: ", i + 1);
+scanf("%d %d %d", &proc[i].pid, &proc[i].burst_time, &proc[i].at);
+}
+fcfsScheduling(proc, n);
 }
