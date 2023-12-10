2ce9732a37a3fe31b8ce2b76ff24c742f23d5742
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-12-10 13:04:38 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h>
 typedef struct{
- int pid;
- int btime;
- int wtime;
- int tatime;
-}sp;
-int main(){
- int i, j, n;
- float total_waiting_time=0, total_turnaround_time=0;
- sp *p,t;
- printf("Enter the no. of processes: ");
+ int at,bt,wt,tat,ct,status;
+}process;
+int main()
+{
+ int n,i; 
+ int totalTAT=0,totalWT=0,completed=0,currentTime=0;
+ printf("Enter the number of processes: ");
  scanf("%d",&n);
- p=(sp*)malloc(n*sizeof(sp));
- printf("Enter the burst time of");
- for(i=0;i<n;i++){
- printf("\nProcess%d\t",i+1);
- scanf("%d",&p[i].btime);
- p[i].pid = i+1;
+ process a[n];
+ printf("Enter the arrival time and burst time\n");
+ int sum=0;
+ for(i=0;i<n;i++)
+ {
+ scanf("%d",&a[i].at);
+ scanf("%d",&a[i].bt);
+ sum+=a[i].bt;
+ a[i].status=0;
  }
- for(i=0;i<n;i++){
- for(j=0;j<n-i-1;j++){
- if(p[j].btime>p[j+1].btime){
- t=p[j];
- p[j]=p[j+1];
- p[j+1]=t;
+ while(completed<n)
+ {
+ int sJob=-1;
+ int sBt = INT_MAX;
+ for(int i=0;i<n;i++)
+ {
+ if(a[i].status==0 && a[i].bt<sBt && a[i].at<=currentTime)
+ {
+ sJob = i;
+ sBt = a[i].bt;
  }
  }
- } 
- p[0].wtime = 0;
- for(i=1;i<n;i++){
- p[i].wtime=0;
- for(j=0;j<i;j++){
- p[i].wtime+=p[j].btime;
+ if(sJob==-1)
+  {
+ currentTime++;
  }
- total_waiting_time+=p[i].wtime;
+ else
+ {
+ a[sJob].ct=currentTime+a[sJob].bt;
+ currentTime = a[sJob].ct;
+ a[sJob].tat = a[sJob].ct-a[sJob].at;
+ a[sJob].wt = a[sJob].tat-a[sJob].bt;
+ totalTAT+=a[sJob].tat;
+ totalWT+=a[sJob].wt;
+ completed++;
+ a[sJob].status=1;
  }
- printf("\n|-----------------------Process Scheduling-----------------------|");
- printf("\n|Process\t|Burst Time\t|Waiting Time\t|Turn Around Time|");
- printf("\n|----------------------------------------------------------------|");
- for(i=0;i<n;i++){
- p[i].tatime = p[i].wtime + p[i].btime;
- total_turnaround_time += p[i].tatime;
- printf("\n|%d\t\t|%d",p[i].pid,p[i].btime);
- printf("\t\t|%d\t\t|%d\t\t |",p[i].wtime,p[i].tatime);
  }
- printf("\n|----------------------------------------------------------------|");
- printf("\n Total Waiting Time: %.2f", total_waiting_time);
- printf("\n Average Waiting Time: %.2f", (float)total_waiting_time / n);
- printf("\n Total Turn Around Time: %.2f", total_turnaround_time);
- printf("\n Average Turn Around Time: %.2f", (float)total_turnaround_time / n);
+float avgTAT = (float)totalTAT/n;
+ float avgWT = (float)totalWT/n;
+ printf("\nPNo\tAT\tBT\tCT\tTAT\tWT\n");
+ for(i=0;i<n;i++)
+ {
+ printf("P%d\t%d\t%d\t%d\t%d\t%d\n",i+1,a[i].at,a[i].bt,a[i].ct,a[i].tat,a[i].wt);
+ }
+ printf("\n");
+ printf("Average Waiting time = %f\n",avgWT);
+ printf("Average Turn Around time = %f\n",avgTAT);
 }
