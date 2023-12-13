3af911e9b4e6d83025e2bb4a3fa98c4d5fed4e95
MANIKANDAN S <102538684+manikandans007@users.noreply.github.com>
2023-12-13 07:25:47 +0530

    Update HERIRARCHICAL.C

--- OS/HERIRARCHICAL.C
+++ OS/HERIRARCHICAL.C
@@ -1,67 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
-#define MAX_NAME_LENGTH 50
-#define MAX_FILES 100
-typedef struct File {
-char name[MAX_NAME_LENGTH];
-char content[1024];
-} File;
-typedef struct Directory {
-char name[MAX_NAME_LENGTH];
-struct Directory* subdirectories[MAX_FILES];
-File files[MAX_FILES];
-int num_subdirectories;
-int num_files;
-} Directory;
-void createDirectory(Directory* parent, const char* name) {
-if (parent->num_subdirectories < MAX_FILES) {
-Directory* newDirectory = (Directory*)malloc(sizeof(Directory));
-strcpy(newDirectory->name, name);
-newDirectory->num_subdirectories = 0;
-newDirectory->num_files = 0;
-parent->subdirectories[parent->num_subdirectories++] = newDirectory;
-printf("Directory '%s' created.\n", name);
-} else
-printf("Directory limit reached. Cannot create '%s'.\n", name);
+
+struct node
+{
+    char name[128];
+    bool isDir;
+    struct node *p;
+    struct node *c[100];
+    int i;
+} * head, *curr;
+
+void ls()
+{
+    if (curr->i == 0)
+    {
+        printf("\nEmpty directory!");
+        return;
+    }
+    for (int i = 0; i < curr->i; i++)
+    {
+        if (curr->c[i]->isDir)
+            printf("\n*%s*  ", curr->c[i]->name);
+        else
+            printf("\n%s  ", curr->c[i]->name);
+    }
 }
-void createFile(Directory* parent, const char* name, const char* content) {
-if (parent->num_files < MAX_FILES) {
-File newFile;
-strcpy(newFile.name, name);
-strcpy(newFile.content, content);
-parent->files[parent->num_files++] = newFile;
-printf("File '%s' created.\n", name);
-} else
-printf("File limit reached. Cannot create '%s'.\n", name);
+
+void touch(bool d)
+{
+    printf("\nEnter Name:");
+    char fname[128];
+    scanf("%s", fname);
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    strcpy(temp->name, fname);
+    temp->isDir = d;
+    temp->p = curr;
+    curr->c[curr->i] = temp;
+    curr->i = (curr->i) + 1;
 }
-void listContents(Directory* dir) {
-printf("Contents of directory '%s':\n", dir->name);
-printf("Subdirectories:\n");
-for (int i = 0; i < dir->num_subdirectories; i++) {
-printf("- %s\n", dir->subdirectories[i]->name);
+
+void cd()
+{
+    printf("\nEnter Directory Name:");
+    char dname[128];
+    scanf("%s", dname);
+    for (int i = 0; i < curr->i; i++)
+    {
+        if (!strcmp(curr->c[i]->name, dname) && curr->c[i]->isDir == true)
+        {
+            curr = curr->c[i];
+            return;
+        }
+    }
+    printf("\nDirectory Not Found!");
 }
-printf("Files:\n");
-for (int i = 0; i < dir->num_files; i++) {
-printf("- %s\n", dir->files[i].name);
+
+void cdup()
+{
+    if (curr->p == NULL)
+    {
+        printf("\nYou are at the Root Directory");
+        return;
+    }
+    curr = curr->p;
 }
+
+void rm(bool d)
+{
+    printf("\nEnter Name of File or Directory to Delete:");
+    char name[128];
+    scanf("%s", name);
+    for (int i = 0; i < curr->i; i++)
+    {
+        if (!strcmp(curr->c[i]->name, name) && ((d && curr->c[i]->isDir == true) || (!d && curr->c[i]->isDir == false)))
+        {
+            int t = i;
+            while (t < (curr->i) - 1)
+            {
+                curr->c[t] = curr->c[t + 1];
+                t++;
+            }
+            curr->i = (curr->i) - 1;
+            printf("\nSuccessfully Deleted.");
+            return;
+        }
+    }
+    printf("\nNot found");
 }
-int main() {
-Directory root;
-strcpy(root.name, "root");
-root.num_subdirectories = 0;
-root.num_files = 0;
-char dirName[MAX_NAME_LENGTH];
-printf("Enter the name of the directory: ");
-scanf("%s", dirName);
-createDirectory(&root, dirName);
-char fileName[MAX_NAME_LENGTH];
-printf("Enter the name of a file: ");
-scanf("%s", fileName);
-char fileContent[1024];
-printf("Enter the content of the file: ");
-scanf("%s", fileContent);
-createFile(&root, fileName, fileContent);
-listContents(&root);
-return 0;
+
+void main()
+{
+    int in;
+    head = (struct node *)malloc(sizeof(struct node));
+    strcpy(head->name, "root");
+    head->isDir = true;
+    head->p = NULL;
+    head->i = 0;
+    curr = head;
+    while (true)
+    {
+        printf("\nYou are in %s directory.\n*****************************************\n1.Show everything in this Directory\n2.Change Directory\n3.Go to Parent Directory\n4.Create New File\n5.Delete File\n6.Create New Directory\n7.Delete Directory\n8.Exit\nEnter your choice:", curr->name);
+        scanf("%d", &in);
+        switch (in)
+        {
+        case 1:
+            ls();
+            break;
+        case 2:
+            cd();
+            break;
+        case 3:
+            cdup();
+            break;
+        case 4:
+            touch(false);
+            break;
+        case 5:
+            rm(false);
+            break;
+        case 6:
+            touch(true);
+            break;
+        case 7:
+            rm(true);
+            break;
+        default:
+            exit(0);
+        }
+    }
 }
